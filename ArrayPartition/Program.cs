5dbc21083c3d41ffceb136094a98ae6d3ac7dd7e
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-12-09 15:50:45 -0500

    added files

---
+++ ArrayPartition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayPartition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int[] nums = { 1,4,3,2};
+            ArrayPairSum(nums);
+        }
+        public static int ArrayPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                sum += nums[i];
+            }
+            return sum;
+        }
+    }
+}
